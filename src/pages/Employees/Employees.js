c9beb047c3afe9079f9663f75b9e27507e5841a7
DotnetMob <dotnetmob@gmail.com>
2020-08-12 16:45:32 +0530

    master

---
+++ src/pages/Employees/Employees.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import EmployeeForm from "./EmployeeForm";
+import PageHeader from "../../components/PageHeader";
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+import { Paper, makeStyles, TableBody, TableRow, TableCell, Toolbar, InputAdornment } from '@material-ui/core';
+import useTable from "../../components/useTable";
+import * as employeeService from "../../services/employeeService";
+import Controls from "../../components/controls/Controls";
+import { Search } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+    pageContent: {
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    },
+    searchInput: {
+        width: '75%'
+    }
+}))
+
+
+const headCells = [
+    { id: 'fullName', label: 'Employee Name' },
+    { id: 'email', label: 'Email Address (Personal)' },
+    { id: 'mobile', label: 'Mobile Number' },
+    { id: 'department', label: 'Department', disableSorting: true },
+]
+
+export default function Employees() {
+
+    const classes = useStyles();
+    const [records, setRecords] = useState(employeeService.getAllEmployees())
+    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
+
+    const {
+        TblContainer,
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
+    } = useTable(records, headCells, filterFn);
+
+    const handleSearch = e => {
+        let target = e.target;
+        setFilterFn({
+            fn: items => {
+                if (target.value == "")
+                    return items;
+                else
+                    return items.filter(x => x.fullName.toLowerCase().includes(target.value))
+            }
+        })
+    }
+
+    return (
+        <>
+            <PageHeader
+                title="New Employee"
+                subTitle="Form design with validation"
+                icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+            />
+            <Paper className={classes.pageContent}>
+                {/* <EmployeeForm /> */}
+                <Toolbar>
+                    <Controls.Input
+                        label="Search Employees"
+                        className={classes.searchInput}
+                        InputProps={{
+                            startAdornment: (<InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>)
+                        }}
+                        onChange={handleSearch}
+                    />
+                </Toolbar>
+                <TblContainer>
+                    <TblHead />
+                    <TableBody>
+                        {
+                            recordsAfterPagingAndSorting().map(item =>
+                                (<TableRow key={item.id}>
+                                    <TableCell>{item.fullName}</TableCell>
+                                    <TableCell>{item.email}</TableCell>
+                                    <TableCell>{item.mobile}</TableCell>
+                                    <TableCell>{item.department}</TableCell>
+                                </TableRow>)
+                            )
+                        }
+                    </TableBody>
+                </TblContainer>
+                <TblPagination />
+            </Paper>
+        </>
+    )
+}
---
+++ src/services/employeeService.js
@@ -0,0 +1,38 @@
+const KEYS = {
+    employees: 'employees',
+    employeeId: 'employeeId'
+}
+
+export const getDepartmentCollection = () => ([
+    { id: '1', title: 'Development' },
+    { id: '2', title: 'Marketing' },
+    { id: '3', title: 'Accounting' },
+    { id: '4', title: 'HR' },
+])
+
+export function insertEmployee(data) {
+    let employees = getAllEmployees();
+    data['id'] = generateEmployeeId()
+    employees.push(data)
+    localStorage.setItem(KEYS.employees, JSON.stringify(employees))
+}
+
+export function generateEmployeeId() {
+    if (localStorage.getItem(KEYS.employeeId) == null)
+        localStorage.setItem(KEYS.employeeId, '0')
+    var id = parseInt(localStorage.getItem(KEYS.employeeId))
+    localStorage.setItem(KEYS.employeeId, (++id).toString())
+    return id;
+}
+
+export function getAllEmployees() {
+    if (localStorage.getItem(KEYS.employees) == null)
+        localStorage.setItem(KEYS.employees, JSON.stringify([]))
+    let employees = JSON.parse(localStorage.getItem(KEYS.employees));
+    //map departmentID to department title
+    let departments = getDepartmentCollection();
+    return employees.map(x => ({
+        ...x,
+        department: departments[x.departmentId - 1].title
+    }))
+}
